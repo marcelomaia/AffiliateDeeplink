8b2b13525276e8240de84d98f8a64b1f773c11fd
Marcelo Maia <mmaia.cc@gmail.com>
2019-05-06 22:15:03 -0300

    import easier

---
+++ affiliate_deeplink/__init__.py
@@ -0,0 +1,5 @@
+from .affiliate.afilio import Afilio
+from .affiliate.amazon import Amazon
+from .affiliate.b2w import B2w
+from .affiliate.magazine_luiza import Magalu
+from .affiliate.natura import Natura
