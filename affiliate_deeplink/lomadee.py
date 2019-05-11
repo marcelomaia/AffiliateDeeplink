9442f217588d5a0c6172dd32a1f7e917d6cc300d
Marcelo Maia <mmaia.cc@gmail.com>
2019-05-11 10:46:18 -0300

    lomadee added.

--- affiliate_deeplink/lomadee.py
+++ affiliate_deeplink/lomadee.py
@@ -1,7 +1,11 @@
+import logging
+
 import requests
 
 from .base import BaseDeeplinkGenerator
 from .config import LOMADEE_SOURCE_ID
+
+logger = logging.getLogger('deeplink.lomadee')
 
 
 class Lomadee(BaseDeeplinkGenerator):
@@ -11,8 +15,15 @@ class Lomadee(BaseDeeplinkGenerator):
 
     @classmethod
     def get_tracking_url(cls, url):
+        deeplink = ''
         req_url = 'http://bws.buscape.com/service/createLinks/lomadee/{app_id}/' \
                   '?sourceId={source_id}&format=json&link1={link_1}'. \
             format(source_id=LOMADEE_SOURCE_ID, link_1=url, app_id='3651516a44624e526551453d')
-        r = requests.get(req_url)
-        return r.json()['lomadeelinks'][0]['lomadeelink']['redirectlink']
+        try:
+            r = requests.get(req_url)
+            deeplink = r.json()['lomadeelinks'][0]['lomadeelink']['redirectlink']
+        except IndexError as e:
+            logger.error('Error: {}'.format(r.text))
+        except ConnectionError as e:
+            logger.error('Error: {}'.format(e))
+        return deeplink
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='affiliate_deeplink',
-    version='0.0.9',
+    version='0.1.0',
     packages=['affiliate_deeplink'],
     url='https://github.com/marcelomaia/AffiliateDeeplink',
     license='MIT',
