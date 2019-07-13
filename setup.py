07d315d24ff3eb2f7556e3e6e4cebd97a14794a2
Marcelo Maia <mmaia.cc@gmail.com>
2019-07-13 20:10:16 -0300

    version 0.1.4

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='affiliate_deeplink',
-    version='0.1.3',
+    version='0.1.4',
     packages=['affiliate_deeplink'],
     url='https://github.com/marcelomaia/AffiliateDeeplink',
     license='MIT',
@@ -10,7 +10,6 @@ setup(
     author_email='mmaia.cc@gmail.com',
     description='Deeplink generator for affiliate marketing.',
     install_requires=[
-        'requests-cache==0.5.0',
-        'requests==2.21.0',
+        'requests>=2.21.0',
     ],
 )
